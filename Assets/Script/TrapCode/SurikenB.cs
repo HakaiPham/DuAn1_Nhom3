75e7fd3936a11dc75c39d16f37cc770be21cb09d
Ling Qin Ru Xue <Hatsuneyato11@gmail.com>
2024-07-11 16:05:09 +0700

    Traps

---
+++ Assets/Script/TrapCode/SurikenB.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurikenB : MonoBehaviour
+{
+    public float speed = 5f;  // Tốc độ di chuyển của SurikenB
+    public float topBoundary = 5f;  // Giới hạn trên
+    public float bottomBoundary = -5f;  // Giới hạn dưới
+
+    private bool movingUp = true;
+
+    void Update()
+    {
+        // Kiểm tra hướng di chuyển và cập nhật vị trí
+        if (movingUp)
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+            if (transform.position.y >= topBoundary)
+            {
+                movingUp = false;
+            }
+        }
+        else
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+            if (transform.position.y <= bottomBoundary)
+            {
+                movingUp = true;
+            }
+        }
+    }
+}
