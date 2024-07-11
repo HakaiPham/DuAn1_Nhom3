9b963e8ff36a18f3bd5d688f1573bca994a5263c
HakaiPham <169151162+HakaiPham@users.noreply.github.com>
2024-07-11 18:08:10 +0700

    Đang hoàn thiện Boss 2, làm quái triệu hồi

--- Assets/Script/BossTank.cs
+++ Assets/Script/BossTank.cs
@@ -16,6 +16,16 @@ public class Boss : MonoBehaviour
     float distancePlayer;
     Rigidbody2D rb;
     BoxCollider2D boxCollider2D;
+    [SerializeField] private GameObject _BossBullet;
+    public Transform _BossAttackTransform;
+    [SerializeField] private GameObject _Metorite;
+    [SerializeField] private GameObject _GateSummon;
+    public float skill1Cooldown = 1f; // Cooldown cho skill đánh thường
+    public float skill2Cooldown = 20f; // Cooldown cho skill 2
+    public float skill3Cooldown = 15f; // Cooldown cho skill 3
+    private float nextSkill1Time;
+    private float nextSkill2Time;
+    private float nextSkill3Time;
     void Start()
     {
         // Lưu trữ scale ban đầu của Boss
@@ -40,7 +50,7 @@ public class Boss : MonoBehaviour
             if (distancePlayer <= _DetectionRange) // Nếu mà vị trí của người nhỏ hơn phạm vi
                                                    // di chuyển của Boss thì Boss sẽ tiến hành xác định điều kiện tiếp theo
             {
-                animator.SetTrigger("IsBossTankIdle");
+                animator.SetBool("IsBossRun", false);
                 Debug.Log("Đã phát hiện người chơi nằm trong phạm vi");
                 if (distancePlayer > _AttackRange) // Nếu player không nằm trong phạm vi tấn công của Boss
                 {
@@ -59,7 +69,7 @@ public class Boss : MonoBehaviour
     {
         //animator.SetBool("isBossRun", true);
         // Tính toán hướng tới người chơi nhưng chỉ trên trục X
-        animator.SetBool("IsBossTankRun", true);
+        animator.SetBool("IsBossRun", true);
         Vector3 targetPosition = new Vector3(player.position.x, transform.position.y, transform.position.z);
 
         // Di chuyển Boss tới vị trí mới
@@ -76,34 +86,107 @@ public class Boss : MonoBehaviour
             transform.localScale = new Vector3(-Mathf.Abs(localScale.x), localScale.y, localScale.z);
         }
     }
-    public void BossSkill4()
+    public int randomskillPercent()
     {
-        animator.SetTrigger("IsBossTankSkill4");
+        int randomSkill = Random.Range(0, 101);
+        if (randomSkill <= 70)
+        {
+            // 80% xác suất cho skill 1 (đánh thường)
+            return 1;
+        }
+        else if (randomSkill <= 90)
+        {
+            // 10% xác suất cho skill 3
+            return 3;
+        }
+        else
+        {
+            // 10% xác suất cho skill 2
+            return 2;
+        }
     }
     public void BossSkillAttack()
     {
-        int randomSkill = Random.Range(1, 4);
+        int skill = randomskillPercent();
+        Debug.Log("Hiện tại skill nằm trong là: " + skill);
         _AttackBossStart -= Time.deltaTime;
         if (_AttackBossStart <= 0)
         {
-            switch (randomSkill)
+            Debug.Log("Dã chạy");
+            switch (skill)
             {
                 case 1:
-                    animator.SetTrigger("IsBossTankSkill1");
+                    if(Time.time >= nextSkill1Time)
+                    {
+                        Debug.Log(">>>>>>>>>>>>>1");
+                        animator.SetBool("IsBossAttack", true);
+                        nextSkill1Time = Time.time + skill1Cooldown;
+                    }                  
                     break;
-                case 2:                   
-                    animator.SetTrigger("IsBossTankSkill2");                 
+                case 2:
+                    if (Time.time >= nextSkill2Time)
+                    {
+                        Debug.Log(">>>>>>>>>>>>>2");
+                        animator.SetBool("IsBossAttack2", true);
+                        nextSkill2Time = Time.time + skill2Cooldown;
+                    }                   
                     break;
                 case 3:
-                    animator.SetTrigger("IsBossTankSkill3");
+                    if(Time.time >= nextSkill3Time)
+                    {
+                        animator.SetBool("IsBossAttack3", true);
+                        nextSkill3Time = Time.time + skill3Cooldown;
+                    }
                     break;
             }
             _AttackBossStart = _AttackBossCoolDown;
         }
-        else if (_AttackBossStart > 0) //Nếu còn đang trong thời gian cooldown
+    }
+    public void BossNormalAttack()
+    {
+        Debug.Log("Đã tạo ra viên đạn");
+        var createBullet = Instantiate(_BossBullet, _BossAttackTransform.position, Quaternion.identity);
+        var speedBullet = new Vector2(3f, 0);
+        createBullet.transform.localScale = new Vector3(6, 6, 6);
+        if (transform.localScale.x < 0)
         {
-            animator.SetTrigger("IsBossTankIdle");
-            animator.SetBool("IsBossTankRun", false);
+            speedBullet = new Vector2(-2f, 0);
+            createBullet.transform.localScale = new Vector3(-6, -6, -6);
         }
+        createBullet.GetComponent<Rigidbody2D>().velocity = speedBullet;
+        Destroy(createBullet, 2f);
+    }
+    public void EndNormalAttack()
+    {
+        animator.SetBool("IsBossAttack", false);
+    }
+    public void EndSkill1()
+    {
+        animator.SetBool("IsBossAttack2", false);
+    }
+    public void EndSkill2()
+    {
+        animator.SetBool("IsBossAttack3", false);
+    }
+    public void CreateMetorite()//Đạn cho skill 
+    {
+        StartCoroutine(WaitCreateMetorite());
+    }
+    IEnumerator WaitCreateMetorite()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var randomPostion = new Vector2(Random.Range(-7.69f, 7.32f), 6);
+            var createMetorite = Instantiate(_Metorite, randomPostion, Quaternion.Euler(0, 0, -90.322f));
+            var speedfall = new Vector2(0, -3f);
+            createMetorite.GetComponent<Rigidbody2D>().velocity = speedfall;
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+    public void CreateGateSummon()
+    {
+        var randomPostion = new Vector2(Random.Range(-7.59f, 6.17f), 0.66f);
+        var createMetorite = Instantiate(_GateSummon, randomPostion, Quaternion.identity);
+        Destroy(createMetorite, 3f);
     }
 }
---
+++ Assets/Script/GateSummon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateSummon : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField] private GameObject _Enemy;
+    public Transform _GateTransform;
+    private BoxCollider2D _BoxColliderEnemy;
+    private Rigidbody2D _RigidbodyEnemy;
+    private GameObject createMonster;
+    public LayerMask groundLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    }
+    public void OpenGateSummon()
+    {
+        animator.SetBool("IsOpenGate", true);
+        createMonster = Instantiate(_Enemy, _GateTransform.position, Quaternion.identity);
+        createMonster.transform.localScale = new Vector3(2, 2, 2);
+        // Lấy các thành phần từ quái vật mới tạo ra
+        _BoxColliderEnemy = createMonster.GetComponent<BoxCollider2D>();
+        _RigidbodyEnemy = createMonster.GetComponent<Rigidbody2D>();
+        if (_BoxColliderEnemy == null || _RigidbodyEnemy == null)
+        {
+            Debug.LogError("Quái vật mới tạo ra không có BoxCollider2D hoặc Rigidbody2D.");
+        }
+        _RigidbodyEnemy.velocity = Vector3.down * 1;
+
+    }
+}
