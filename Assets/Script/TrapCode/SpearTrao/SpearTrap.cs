562ee8845e15f20320724ab086e1efb99c8bc648
Ling Qin Ru Xue <Hatsuneyato11@gmail.com>
2024-07-13 12:39:26 +0700

    SpearTrap

---
+++ Assets/Script/TrapCode/SpearTrao/SpearTrap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearTrap : MonoBehaviour
+{
+  private CapsuleCollider2D _SpearTrapCollider2d;
+    void Start()
+    {
+        // Gán CapsuleCollider2D của SpearTrap khi bắt đầu
+        _SpearTrapCollider2d = GetComponent<CapsuleCollider2D>();
+
+    }
+    public void DisableColider()
+    {
+        _SpearTrapCollider2d.enabled = false;
+    }
+
+    public void EnableColider()
+    {
+        _SpearTrapCollider2d.enabled = true;
+    }
+
+
+}
