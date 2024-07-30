59b21de86c4083522a160877108f467f03f51fd2
Ling Qin Ru Xue <Hatsuneyato11@gmail.com>
2024-07-30 15:16:59 +0700

    Submit UI

---
+++ Assets/Script/UI/Menu/GameOverMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject gameOverMenu;
+    [SerializeField] private GameObject CheckPlayer;
+
+    void Update()
+    {
+        // Kiểm tra xem người chơi còn tồn tại không
+        if (CheckPlayer == null)
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void GotoHome()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Login");
+    }
+}
